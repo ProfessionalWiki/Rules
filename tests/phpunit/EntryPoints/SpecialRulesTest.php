db925f14cbb206dac4c2baf1b06323d9aa8ea705
alistair3149 <alistair31494322@gmail.com>
2025-07-08 15:39:01 -0400

    Add Special:Rules page

---
+++ tests/phpunit/EntryPoints/SpecialRulesTest.php
@@ -0,0 +1,38 @@
+<?php
+
+declare( strict_types = 1 );
+
+namespace ProfessionalWiki\Rules\Tests\EntryPoints;
+
+use ProfessionalWiki\Rules\EntryPoints\SpecialRules;
+use ProfessionalWiki\Rules\RulesExtension;
+use SpecialPageTestBase;
+
+/**
+ * @covers \ProfessionalWiki\Rules\EntryPoints\SpecialRules
+ */
+class SpecialRulesTest extends SpecialPageTestBase {
+
+	/**
+	 * @inheritDoc
+	 */
+	protected function newSpecialPage(): SpecialRules {
+		return $this->getServiceContainer()->getSpecialPageFactory()->getPage( 'Rules' );
+	}
+
+	/**
+	 * @covers \ProfessionalWiki\Rules\EntryPoints\SpecialRules::execute
+	 */
+	public function testRedirect(): void {
+		$specialRules = $this->newSpecialPage();
+		$specialRules->execute( null );
+		$this->assertEquals( $this->getRedirectTargetUrl(), $specialRules->getOutput()->getRedirect() );
+	}
+
+	private function getRedirectTargetUrl(): string {
+		return $this->getServiceContainer()->get( 'TitleFactory' )->newFromText(
+			RulesExtension::RULES_PAGE_TITLE, NS_MEDIAWIKI
+		)->getFullURL();
+	}
+
+}
