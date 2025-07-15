e4f0c589a772c7c98cd4729171fd559659d3a769
alistair3149 <alistair31494322@gmail.com>
2025-07-14 23:07:43 -0400

    Add Rules table component
    #6

--- modules/ext.rules/init.js
+++ modules/ext.rules/init.js
@@ -8,10 +8,11 @@ const
 function initApp() {
 	const targetElement = document.getElementById( 'ext-rules-app' );
 	if ( targetElement === null ) {
-		mw.log.error( '[ext.rules] Unable to mount Vue app: #ext-rules-app element not found' );
+		mw.log.warn( '[ext.rules] Unable to mount Vue app: #ext-rules-app element not found' );
 		return;
 	}
 
+	// @ts-ignore createMwApp is a MediaWiki-specific function
 	const app = Vue.createMwApp( App, {} );
 	app.mount( targetElement );
 }
