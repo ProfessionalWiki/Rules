cf45e4833533f6d34f09ecf68318edc76f8c4bf4
alistair3149 <alistair31494322@gmail.com>
2025-07-25 17:29:21 -0400

    Drop ContentHandler approach

--- src/EntryPoints/RulesHooks.php
+++ src/EntryPoints/RulesHooks.php
@@ -4,68 +4,45 @@ declare( strict_types = 1 );
 
 namespace ProfessionalWiki\Rules\EntryPoints;
 
-use MediaWiki\CommentStore\CommentStoreComment;
-use MediaWiki\Html\Html;
-use MediaWiki\Output\OutputPage;
-use MediaWiki\Page\Hook\ShowMissingArticleHook;
+use MediaWiki\Content\JsonContent;
 use MediaWiki\Content\Hook\ContentAlterParserOutputHook;
-use MediaWiki\Output\Hook\BeforePageDisplayHook;
+use MediaWiki\Html\Html;
+use MediaWiki\Page\Hook\ShowMissingArticleHook;
 use MediaWiki\Revision\Hook\ContentHandlerDefaultModelForHook;
-use MediaWiki\Revision\SlotRecord;
-use MediaWiki\Title\Title;
 use ProfessionalWiki\Rules\RulesExtension;
 
+class RulesHooks implements ContentAlterParserOutputHook, ContentHandlerDefaultModelForHook, ShowMissingArticleHook {
 
-class RulesHooks implements BeforePageDisplayHook, ContentAlterParserOutputHook, ContentHandlerDefaultModelForHook, ShowMissingArticleHook {
-
-	public function onBeforePageDisplay( $out, $skin ): void {
-		$title = $out->getTitle();
-
-		if (
-			$title === null ||
-			$out->getActionName() !== 'view' ||
-			!RulesExtension::getInstance()->isRulesPage( $title )
-		) {
-			return;
+	public function onContentAlterParserOutput( $content, $title, $parserOutput ) {
+		if ( !RulesExtension::getInstance()->isRulesPage( $title ) ) {
+			RulesExtension::getInstance()->newApplyRulesUseCase()->applyToPage( $parserOutput );
+		} else {
+			/** @var JsonContent $content */
+			$parserOutput->setJsConfigVar( 'rules', $content->getData() );
+			$parserOutput->setRawText( $this->getRulesPageHtml() );
+			$parserOutput->addModuleStyles( [ 'ext.rules.styles' ] );
+			$parserOutput->addModules( [ 'ext.rules' ] );
 		}
-
-		$out->addHTML( '<div id="ext-rules-app"></div>' );
-		$out->addModules( 'ext.rules' );
 	}
 
 	public function onContentHandlerDefaultModelFor( $title, &$model ): void {
 		if ( RulesExtension::getInstance()->isRulesPage( $title ) ) {
-			$model = RulesExtension::RULES_CONTENT_MODEL;
+			$model = CONTENT_MODEL_JSON;
 		}
 	}
 
 	public function onShowMissingArticle( $article ): void {
-		$rulesExtension = RulesExtension::getInstance();
-		$title = $article->getTitle();
-
-		if ( !$rulesExtension->isRulesPage( $title ) ) {
-			return;
+		if ( RulesExtension::getInstance()->isRulesPage( $article->getTitle() ) ) {
+			$output = $article->getContext()->getOutput();
+			$output->addHtml( $this->getRulesPageHtml() );
+			$output->addModuleStyles( [ 'ext.rules.styles' ] );
+			$output->addModules( [ 'ext.rules' ] );
 		}
-
-		// Fill the rules page with the default content
-		$this->createRulesPage( $rulesExtension, $title );
-		// Render a message to prompt the user to refresh the page
-		// Because the fillParserOutput method is not called when the page is first created
-		$this->renderRefreshMessage( $article->getContext()->getOutput() );
 	}
 
-	private function createRulesPage( RulesExtension $rulesExtension, Title $title ): void {
-		$pageUpdater = $rulesExtension->getRulesPageUpdater( $title );
-		$pageUpdater->setContent( SlotRecord::MAIN, new RulesContent() );
-		$pageUpdater->saveRevision( CommentStoreComment::newUnsavedComment( $rulesExtension->getEditCommentForRulesPageInit() ) );
-	}
-
-	private function renderRefreshMessage( OutputPage $output ): void {
-		$output->addHTML( Html::noticeBox( $output->msg( 'rules-refresh-message' )->text(), [] ) );
-	}
-
-	public function onContentAlterParserOutput( $content, $title, $parserOutput ) {
-		RulesExtension::getInstance()->newApplyRulesUseCase()->applyToPage( $parserOutput );
+	private function getRulesPageHtml(): string {
+		return Html::element( 'div', [ 'id' => 'ext-rules-app' ] ) .
+			Html::rawElement( 'noscript', [], Html::noticeBox( wfMessage( 'rules-noscript-message' )->text(), [] ) );
 	}
 
 }
