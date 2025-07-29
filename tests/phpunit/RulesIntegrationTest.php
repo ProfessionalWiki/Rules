aa5f7f0305f1cc82c0d3faa2cb580f7f0a5a5c72
Morne Alberts <morne@malberts.net>
2025-07-29 15:54:51 +0200

    Trigger links update in maintenance script

--- tests/phpunit/RulesIntegrationTest.php
+++ tests/phpunit/RulesIntegrationTest.php
@@ -7,6 +7,7 @@ namespace ProfessionalWiki\Rules\Tests;
 use MediaWiki\Title\Title;
 use MediaWikiIntegrationTestCase;
 use ProfessionalWiki\Rules\RulesExtension;
+use WikiPage;
 
 class RulesIntegrationTest extends MediaWikiIntegrationTestCase {
 
@@ -14,6 +15,31 @@ class RulesIntegrationTest extends MediaWikiIntegrationTestCase {
 		$this->editPage(
 			Title::newFromText( RulesExtension::RULES_PAGE_TITLE, NS_MEDIAWIKI ),
 			$config
+		);
+	}
+
+	protected function createPageWithText( string $text = 'Whatever wikitext' ): WikiPage {
+		$page = $this->getServiceContainer()->getWikiPageFactory()->newFromTitle( $this->createUniqueTitle() );
+
+		$this->editPage( $page, $text );
+
+		return $page;
+	}
+
+	private function createUniqueTitle(): Title {
+		static $pageCounter = 0;
+		return Title::newFromText( 'RulesTestPage' . ++$pageCounter );
+	}
+
+	/**
+	 * @param string[] $categories
+	 */
+	public function assertPageHasCategories( Title $title, array $categories ): void {
+		$parserOutput = $this->getServiceContainer()->getWikiPageFactory()->newFromTitle( $title )->getParserOutput();
+
+		$this->assertSame(
+			$parserOutput === false ? [] : $parserOutput->getCategoryNames(),
+			$categories
 		);
 	}
 
