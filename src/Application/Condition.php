077232156de127492e9208e390736c9b743ae89d
Morne Alberts <morne@malberts.net>
2025-07-31 13:44:19 +0200

    Normalize condition categories

--- src/Application/Condition.php
+++ src/Application/Condition.php
@@ -4,6 +4,8 @@ declare( strict_types = 1 );
 
 namespace ProfessionalWiki\Rules\Application;
 
+use MediaWiki\Title\Title;
+
 /**
  * Currently we only support the "inCategory" condition type
  * which matches as soon as one of the present categories is in the condition.
@@ -22,8 +24,11 @@ class Condition {
 	 * @param string[] $presentCategories
 	 */
 	public function isFulfilled( array $presentCategories ): bool {
-		foreach ( $presentCategories as $category ) {
-			if ( in_array( $category, $this->categoryNames, true ) ) {
+		$normalizedPresentCategories = $this->normalizeCategories( $presentCategories );
+		$normalizedConditionCategories = $this->normalizeCategories( $this->categoryNames );
+
+		foreach ( $normalizedPresentCategories as $category ) {
+			if ( in_array( $category, $normalizedConditionCategories, true ) ) {
 				return true;
 			}
 		}
@@ -31,4 +36,14 @@ class Condition {
 		return $this->categoryNames === [];
 	}
 
+	/**
+	 * @param string[] $categories
+	 * @return string[]
+	 */
+	private function normalizeCategories( array $categories ): array {
+		return array_map(
+			fn( string $category ) => Title::newFromText( $category, NS_CATEGORY )->getDBkey(),
+			$categories
+		);
+	}
 }
